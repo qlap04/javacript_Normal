c51d53a3389ac864d91885fff2f835a8dd9287c1
qlap04 <uchihaquoc04@gmail.com>
2024-09-01 21:18:15 +0700

    object2

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-var myInfo = {
-    name: "Qlap",
-    id: 22162019,
-    age: 20,
-    address: "HCM City",
-    getId: function () {
-        return String(this.id)
+// var myInfo = {
+//     name: "Qlap",
+//     id: 22162019,
+//     age: 20,
+//     address: "HCM City",
+//     getId: function () {
+//         return String(this.id)
+//     }
+// };
+// myInfo.email = "uchihaquoc04@gmail.com"
+// myInfo["my-family"] = "three members"
+// console.log(typeof myInfo.getId())
+
+
+function User(id, name, role, avatar) {
+    this.id = id
+    this.name = name
+    this.role = role
+    this.avatar = avatar
+    this.getName = function () {
+        return `${this.name} ${this.id}`
     }
-};
-myInfo.email = "uchihaquoc04@gmail.com"
-myInfo["my-family"] = "three members"
-console.log(typeof myInfo.getId())
+}
+
+var author = new User(1, "QLap", "Husband", "Avatar")
+author.title = "Share public"
+
+console.log(author.getName())
